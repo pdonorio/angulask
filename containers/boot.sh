8988735f608d65321c2cd0ae814fac5933325326
DAntonio Mattia <m.dantonio81@gmail.com>
2015-11-04 08:49:07 +0000

    Bug fix irods pwd

--- containers/boot.sh
+++ containers/boot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 . /opt/venv/bin/activate
-sleep 10
+sleep 30
 curl -i -X POST \
     http://neo4j:neo4j@neo:7474/user/neo4j/password \
     -d 'password=test'
+
+yes test | iinit
+
 python /app/run.py
